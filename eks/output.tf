7c65742df0700e9a5ff95932201a095290d5ffc8
wlopezob <wlopezob@gmail.com>
2024-10-10 01:44:53 +0000

    deploy api

--- eks/output.tf
+++ eks/output.tf
@@ -1,3 +1,7 @@
 output "aws_loadbalancer_uri" {
   value =  "http://${data.kubernetes_service.ingress_nginx.status.0.load_balancer.0.ingress.0.hostname}"
 }
+
+output "vpc_link_id" {
+  value = aws_api_gateway_vpc_link.vpc-link.id
+}
